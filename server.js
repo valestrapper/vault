54a2c6043c8e11064ef27561c6296afcd0b92d74
valestrapper <115505619+valestrapper@users.noreply.github.com>
2026-05-11 11:48:45 -0400

    Update server.js

--- server.js
+++ server.js
@@ -41,8 +41,7 @@ function collectFileIds(node, ids = []) {
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static(__dirname));
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Multer config - store to disk with uuid filenames
 const storage = multer.diskStorage({
